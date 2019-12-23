0df8414e138353fbfb9a56e354301202c151eab2
Meagan Makinson <meaganmakinson80@gmail.com>
2019-12-22 21:04:01 -0500

    add cityName function and handleSubmit function, error handling

--- weather-checker/main.js
+++ weather-checker/main.js
@@ -1,12 +1,38 @@
+let locationResult = document.getElementById('result');
+let cityname = document.getElementById('search-form').addEventListener('submit', handleSubmit);
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const userInput = document.getElementById("search-input").value;
+    cityName(userInput);
+};
+
+
+async function cityName(cityname) {
+    try {
+        const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${cityname}`);
+        const cityData = await result.json();
+        const cityWoeID = cityData[0].woeid;
+        
+        locationSearch(cityWoeID);
+    } catch(error) {
+        locationResult.innerHTML = `No data is available for this city`;
+        
+    }
+}
+
+
+
 async function locationSearch(woeid) {
     try {
         const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
         const data = await result.json();
-        const today = data.consolidated_weather[0];
-        console.log(`The high and low today in ${data.title} is ${today.min_temp} and ${today.max_temp}`);
+        const min_tempRounded = Math.round(data.consolidated_weather[0].min_temp);
+        const max_tempRounded = Math.round(data.consolidated_weather[0].max_temp);
+        locationResult.innerHTML = `The high and low today in ${data.title} is ${min_tempRounded} celsius and ${max_tempRounded} celsius`;
     } catch(error) {
-        console.log(error);
+        
     }
 }
-    
-locationSearch(2487956);
+
