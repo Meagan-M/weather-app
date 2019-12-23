c0572ae61ca8ea03990a90f039929e233509302c
Meagan Makinson <meaganmakinson80@gmail.com>
2019-12-22 23:03:41 -0500

    add 5 day forecast for a city by looping through array of each day and displaying selected data

--- weather-checker/main.js
+++ weather-checker/main.js
@@ -28,9 +28,29 @@ async function locationSearch(woeid) {
     try {
         const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
         const data = await result.json();
-        const min_tempRounded = Math.round(data.consolidated_weather[0].min_temp);
-        const max_tempRounded = Math.round(data.consolidated_weather[0].max_temp);
-        locationResult.innerHTML = `The high and low today in ${data.title} is ${min_tempRounded} celsius and ${max_tempRounded} celsius`;
+
+        locationResult.innerHTML += `<h3>${data.title} Five-Day Forecast</h3>`;
+        
+        for (let i = 0; i <= data.consolidated_weather.length; i++) {
+            const max_tempRounded = Math.round(data.consolidated_weather[i].max_temp);
+            const date = (data.consolidated_weather[i].applicable_date);
+            const weatherState = (data.consolidated_weather[i].weather_state_name);
+            const min_tempRounded = Math.round(data.consolidated_weather[i].min_temp);
+            
+            console.log(max_tempRounded);
+            locationResult.innerHTML += `<p>${date} ${weatherState} High ${max_tempRounded} Celsius Low ${min_tempRounded} Celsius </p>`;
+        }
+
+        
+        
+        
+        
+        
+        // if (max_tempRounded <= 16) {
+        //     
+        // } else {
+        //     
+        // }
     } catch(error) {
         
     }
